2bc8d92f6fe8a41c6dcf166e0709be3a0cdfbfe9
QidamZola <qidamzolafarhan@gmail.com>
2019-04-18 19:56:10 +0700

    lat2-akb2-10116055-qidamzolafarhan

--- app/src/main/java/com/example/myapplication/userhome.java
+++ app/src/main/java/com/example/myapplication/userhome.java
@@ -3,7 +3,30 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
-
+/*
+ *   AKB2-10116055-Qidam Zola Farhan
+ *
+ *  Change Log Selasa 16 April 2019 10:30
+ *  - membuat login / main activity
+ *
+ *  Change Log Selasa 16 April 2019 11:30
+ *  - membuat register
+ *
+ *  Change Log Selasa 16 April 2019 13:00
+ *  - membuat almost there
+ *
+ *  Change Log Selasa 16 April 2019 14:00
+ *  - membuat verify
+ *
+ *  Change Log Kamis 18 April 2019 16:00
+ *  - membuat user home
+ *
+ *  Change Log Selasa 15 April 2019 18:00
+ *  - edit login
+ *  - edit register
+ *  - edit almost there
+ *  - edit verify
+ * */
 public class userhome extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/myapplication/verify.java
+++ app/src/main/java/com/example/myapplication/verify.java
@@ -6,7 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-
+/*
+ *   AKB2-10116055-Qidam Zola Farhan
+ *
+ *  Change Log Selasa 16 April 2019 10:30
+ *  - membuat login / main activity
+ *
+ *  Change Log Selasa 16 April 2019 11:30
+ *  - membuat register
+ *
+ *  Change Log Selasa 16 April 2019 13:00
+ *  - membuat almost there
+ *
+ *  Change Log Selasa 16 April 2019 14:00
+ *  - membuat verify
+ *
+ *  Change Log Kamis 18 April 2019 16:00
+ *  - membuat user home
+ *
+ *  Change Log Selasa 15 April 2019 18:00
+ *  - edit login
+ *  - edit register
+ *  - edit almost there
+ *  - edit verify
+ * */
 public class verify extends AppCompatActivity {
     private Button button;
     @Override
